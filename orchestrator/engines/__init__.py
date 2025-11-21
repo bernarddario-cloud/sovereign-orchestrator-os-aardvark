b37f3cfd589cfce44de707120f2270730b19bdfe
bernarddario-cloud <bernard.dario@gmail.com>
2025-11-20 22:50:26 -0800

    Create __init__.py

---
+++ orchestrator/engines/__init__.py
@@ -0,0 +1,7 @@
+"""Engines module for Sovereign Orchestrator OS
+
+This module contains pluggable engine implementations for different
+phases of the orchestration workflow.
+"""
+
+__version__ = "0.1.0"
